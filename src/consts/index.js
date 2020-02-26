8e647ead593afff7193603973d379d553a4ca901
mridx <mridulbaishya28@gmail.com>
2020-02-26 21:37:02 +0530

    changed db query

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,6 +1,8 @@
-export const getAllPost = "http://127.0.0.1:3333/api/v1/posts";
-export const login = "http://127.0.0.1:3333/api/v1/login";
-export const me = "http://127.0.0.1:3333/api/v1/me";
-export const categories = "http://127.0.0.1:3333/api/v1/categories";
-export const postByCategory = "http://127.0.0.1:3333/api/v1/posts/category/";
-export const post = "http://127.0.0.1:3333/api/v1/post/";
+export const baseurl = "https://dartmedia.herokuapp.com/api/v1/";
+
+export const getAllPost = baseurl + "posts";
+export const login = baseurl + "login";
+export const me = baseurl + "me";
+export const categories = baseurl + "categories";
+export const postByCategory = baseurl + "posts/category/";
+export const post = baseurl + "post/";
